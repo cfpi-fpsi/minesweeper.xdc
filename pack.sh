2993ee6d50c76395a451c2fc698c18209fd743f7
Emil Kosz <ekosz@int.pl>
2024-08-21 10:58:56 +0200

    Fix bugs in script

--- pack.sh
+++ pack.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-EXCLUDE_FILES=LICENSE README.md webxdc.js *~ *.xdc *.sh
+EXCLUDE_FILES="LICENSE README.md webxdc.js *~ *.xdc *.sh"
 MAX_SIZE=100000			# 100 kilobytes
 
-rm application.xdc
+rm application.xdc > /dev/null
 zip -9 --recurse-paths application.xdc --exclude $EXCLUDE_FILES -- *
 unzip -l application.xdc
 
 # Check package size.
-if [ $(wc -c < application.xdc) -ge $MAX_SIZE ]; then
+if [ "$(wc -c < application.xdc)" -ge $MAX_SIZE ]; then
     echo "WARNING: package size exceeded 100 kilobytes"
 fi
